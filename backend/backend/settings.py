67ebc5e4896efed6252edb6c0c472b8cedf17f75
Vilém Janota <willjanota@gmail.com>
2024-06-26 18:05:10 +0200

    update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,11 +100,12 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
+        'PORT': os.getenv('DB_PORT'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PWD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+
     }
 }
 
